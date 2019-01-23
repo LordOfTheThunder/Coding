e5d2a5004682cdef435cda1a28cce90ad8fce723
Max Kolchinsky <max.kolchinsky@gmail.com>
2019-01-23 22:05:48 +0200

    Added better solution

--- Companies/Google/twoNumsSumUpToK.h
+++ Companies/Google/twoNumsSumUpToK.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <unordered_set>
 using namespace std;
 
 /*
@@ -12,9 +13,8 @@ Example:	For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7
 */
 
 /*
-
 Solution:
-	Complexity: NlogN
+	Complexity: NlogN - worst case
 	Details:	Sort the array and then iterate with two pointers till you find the sum
 	Optimization:
 		1. use iterators to save run time instead of using [] operator
@@ -40,4 +40,32 @@ bool twoNumsSumUpToK(vector<int> arr, int k) {
 		temp2 = *rev_it;
 	}
 	return false;
+}
+
+/*
+	Solution:
+	Complexity: N - Average case
+	Details:	Use a hash
+	Optimization:
+		1. use iterators to save run time instead of using [] operator
+		2. use temp variables to minimize amount of dereferences
+*/
+bool twoNumsSumUpToK_better(vector<int> arr, int k) {
+	unordered_set<int> s;
+	for (vector<int>::iterator it = arr.begin(); it != arr.end(); ++it) {
+		int deref_tmp = *it;
+		int temp = k - deref_tmp;
+		if (s.find(temp) != s.end()) {
+			return true;
+		}
+		s.insert(deref_tmp);
+	}
+	return false;
+}
+
+int main() {
+	vector<int> arr = { 10, 15, 3, 7 };
+	if (twoNumsSumUpToK_better(arr, 17)) {
+		cout << "Success" << endl;
+	}
 }
